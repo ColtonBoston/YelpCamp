cde9edda0ceefb253f4645bfa59b20de4a96ba7d
Colton Boston <coltonbboston@gmail.com>
2017-12-21 13:08:05 -0600

    add start script and change app.listen

--- app.js
+++ app.js
@@ -48,4 +48,4 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen("3000", console.log("listening..."));
+app.listen(process.env.PORT, process.env.IP, console.log("listening..."));
